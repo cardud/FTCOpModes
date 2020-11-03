aad59bae7a734af085ccdac3c811c6aaea054051
cardud <67175738+cardud@users.noreply.github.com>
2020-11-03 14:55:56 -0600

    commited

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpBlue.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+
+
+@TeleOp
+public class TeleOpBlue extends LinearOpMode {
+
+
+    // maybe chnage the names casue FR also is used ofr File Reader i dont think we will need it but
+// the ide keeps inporting it
+
+    private DcMotor BR = null;
+    private DcMotor BL = null;
+    private DcMotor FR = null;
+    private DcMotor FL = null;
+    // please chcek the spelling of the motor names in the config file
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        BR = hardwareMap.dcMotor.get("back_right_motor");
+        BL = hardwareMap.dcMotor.get("back_left_motor");
+        FR = hardwareMap.dcMotor.get("front_right_motor");
+        FL = hardwareMap.dcMotor.get("front_left_motor");
+
+
+        BL.setDirection(DcMotorSimple.Direction.REVERSE);
+        FL.setDirection(DcMotorSimple.Direction.REVERSE);
+        BR.setDirection(DcMotorSimple.Direction.FORWARD);
+        FR.setDirection(DcMotorSimple.Direction.FORWARD);
+
+
+        waitForStart();
+
+        /*
+
+         */
+
+        //double x1 = 0;
+        // double y1 = 0;
+
+
+        while (opModeIsActive()) {
+
+            // x1.setPower(gamepad1.left_stick_x);
+            //  y1.setPower(-gamepad1.left_stick_y);
+
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x*1.5;
+            double rx = gamepad1.right_stick_x;
+
+
+
+
+
+            FL.setPower(y + x + rx) ;
+            BL.setPower(y - x + rx) ;
+            FR.setPower(y - x - rx) ;
+            BR.setPower(y + x - rx) ;
+
+
+            idle();
+
+        }
+
+
+    }
+
+
+}
